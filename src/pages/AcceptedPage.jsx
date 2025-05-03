c7ef3bd6e59803f75968ba8ae680cbd1460393ec
jevgeni <jevgeni@build-game.com>
2025-05-03 20:09:54 +0300

    final polishments

--- src/pages/AcceptedPage.jsx
+++ src/pages/AcceptedPage.jsx
@@ -1,16 +1,51 @@
-export default function AcceptedPage({ guest }) {
+import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { supabase } from '../supabaseClient'
+
+export default function AcceptedPage() {
+    const { code } = useParams()
+    const [guest, setGuest] = useState(null)
+
+    useEffect(() => {
+        const fetchGuest = async () => {
+            const { data } = await supabase
+                .from('wedding_guests')
+                .select('*')
+                .eq('code', code)
+                .single()
+            setGuest(data)
+        }
+
+        fetchGuest()
+    }, [code])
+
+    if (!guest) return <p className="text-center mt-10">Loading...</p>
+
+    function getGreetingPrefix(guest) {
+        if (guest.amount && guest.amount > 1) return 'Дорогие';
+
+        switch (guest.gender.toLowerCase()) {
+            case 'male':
+                return 'Дорогой';
+            case 'female':
+                return 'Дорогая';
+            default:
+                return 'Дорогой гость';
+        }
+    }
+
     return (
         <div className="min-h-screen bg-white text-center p-6">
             <h1 className="text-3xl font-bold mb-4">🎉 Супер!</h1>
-            <p className="text-lg mb-2">Дорогие {guest?.name},</p>
-            <p className="mb-4">Мы очень рады, что вы разделите с нами приздник</p>
+            <p className="text-lg mb-2">{getGreetingPrefix(guest)} {guest.name},</p>
+            <p className="mb-4">Мы очень рады, что вы разделите с нами праздник!</p>
 
             <div className="bg-pink-100 rounded-lg shadow p-4 max-w-xl mx-auto">
                 <h2 className="text-xl font-semibold mb-2">📍 Где</h2>
-                <p>Ресторан Bacio, Narva mnt 63/4, Tallinn</p>
+                <a href="https://bacio.ee/" target="_blank">Ресторан Bacio</a>, <a href="https://maps.app.goo.gl/RLUSEJj9b93xxe1v9" target="_blank">Narva mnt 63/4, Tallinn</a>
 
                 <h2 className="text-xl font-semibold mt-4 mb-2">📅 Когда</h2>
-                <p>Пятница, 16-го мая, 2025 at 17:00</p>
+                <p>Пятница, 16-го мая, 2025 at 17:30</p>
 
                 <h2 className="text-xl font-semibold mt-4 mb-2">👗 Дресс код</h2>
                 <p>Торжественный</p>
@@ -20,6 +55,12 @@ export default function AcceptedPage({ guest }) {
 
                 <h2 className="text-xl font-semibold mt-4 mb-2">📝 Пожалуйста</h2>
                 <p>Подтвердите своё присутствие на торжестве до 5.05.2025</p>
+
+                <h2 className="text-xl font-semibold mt-4 mb-2">📝 Еще</h2>
+                <p>Мы будем рады вам и вашим детям, но уверены, что маленьким гостям будет комфортнее остаться дома, чем быть на шумном празднике</p>
+
+                <h2 className="text-xl font-semibold mt-4 mb-2">📝 Вопросы?</h2>
+                <p>Звоните 5667 0265</p>
             </div>
         </div>
     )
